e8ebd1b9c6470a15e33a44c0875e714530a5b97f
yanlele <331393627@qq.com>
2020-04-28 00:53:16 +0800

    feat: add script

--- student/src/main/java/com/system/student/controller/UserController.java
+++ student/src/main/java/com/system/student/controller/UserController.java
@@ -13,13 +13,13 @@ import java.util.List;
 public class UserController {
 
     private List<User> userList = new ArrayList<>();
-    {
-        userList.add(new User("1", "socks", "123456", new Date()));
-        userList.add(new User("2", "admin", "111111", new Date()));
-        userList.add(new User("3", "jacks", "222222", null));
-    }
+
     @GetMapping("/")
     public ModelAndView index() {
+        userList.add(new User("1", "socks", "123456", new Date()));
+        userList.add(new User("2", "admin", "111111", new Date()));
+        userList.add(new User("2", "admin", "111111", new Date()));
+        userList.add(new User("3", "jacks", "222222", null));
         return new ModelAndView("user/user", "userList", userList);
     }
 }
