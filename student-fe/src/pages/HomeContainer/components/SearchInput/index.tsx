3cb59cb080dbe4fdbcc2e0dfc38aed05c589ee7b
yanlele <331393627@qq.com>
2020-05-01 16:03:46 +0800

    feat: 修改placeholder

--- student-fe/src/pages/HomeContainer/components/SearchInput/index.tsx
+++ student-fe/src/pages/HomeContainer/components/SearchInput/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { Input } from 'antd';
 
 const SearchInput: FC = () => {
-  return <Input placeholder="按学号、姓名、搜索" />;
+  return <Input placeholder="按 学号、姓名 搜索" />;
 };
 
 export default SearchInput;
