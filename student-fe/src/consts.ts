4ea231ad428819072d33c191ff4d2c2f0ebe7510
yanlele <331393627@qq.com>
2020-05-01 19:11:49 +0800

    feat: 添加基础表单

--- student-fe/src/consts.ts
+++ student-fe/src/consts.ts
@@ -1,2 +1,12 @@
 // 公共常量
 export const DATE_FORMAT = 'YYYY-MM-DD';
+export const FORM_ITEM_LAYOUT = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+};
